f19759d6eb7b131ce3d4f5c9ddfad66bba59ed77
hagenconnor <62457713+hagenconnor@users.noreply.github.com>
2022-07-25 20:12:08 -0400

    Add files via upload

---
+++ online_bookstore.sql
@@ -0,0 +1,55 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for macos12 (arm64)
+--
+-- Host: localhost    Database: online_bookstore
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book_list`
+--
+
+DROP TABLE IF EXISTS `book_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book_list` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(90) NOT NULL,
+  `author` varchar(90) NOT NULL,
+  `genre` varchar(30) NOT NULL,
+  `year` int DEFAULT NULL,
+  `reg_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_list`
+--
+
+LOCK TABLES `book_list` WRITE;
+/*!40000 ALTER TABLE `book_list` DISABLE KEYS */;
+INSERT INTO `book_list` VALUES (1,'test23455','Changed_author','test genre',NULL,'2022-07-18 19:21:22'),(2,'Test123','Connor Hagen','Test2Genre',NULL,'2022-07-06 18:48:34'),(5,'Test2134','Connor Hagen','Horror',1982,'2022-07-25 23:51:05'),(6,'Test7','Test Author','Woah',1987,'2022-07-26 00:04:47');
+/*!40000 ALTER TABLE `book_list` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-25 20:11:44
